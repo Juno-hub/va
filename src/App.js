67f1d64e7327f93e4c8824ea4de6b88d75abd8bf
Juno <bae3054@naver.com>
2020-09-09 19:33:45 +0900

    Login form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
 import React from "react";
 
-function App() {
-  return (
-    <div>
-      <h1>hello</h1>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    user_id: "",
+    checkbox: "",
+  };
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form action="/login" method="post" onSubmit={this.handleSubmit}>
+        <label>
+          <div className="user_id">
+            <input
+              type="text"
+              name="user_id"
+              placeholder="이메일 주소 또는 전화번호"
+              required
+            />
+          </div>
+          <div className="password">
+            <input
+              type="password"
+              name="password"
+              placeholder="비밀번호"
+              required
+            />
+          </div>
+        </label>
+        <input type="submit" value="로그인" />
+        <div className="remember">
+          <input type="checkbox" value="checkbox" />
+          <span className="remember__text">로그인 정보 입력</span>
+        </div>
+      </form>
+    );
+  }
 }
 
 export default App;
